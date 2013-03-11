f4d63f32eca70721048035f351ae140e04ff9e71
Nana Kay <christian.osei-bonsu@meltwater.org>
2013-03-11 19:08:58 +0000

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CampusConnect::Application.config.secret_token = '1aa04ed82ff4f15fe70b4473c2bead20f8920678aab75176b7d4273ca3892337a5fdb3a90326f139370999b41d99b8a31461f003c719766928ff784fd81d135a'
